8dde8f4e8696c7514643bced7b8b0b08ca9f4231
Dan Flores <danf425@gmail.com>
2019-08-26 17:01:41 -0400

    remote-exec provisioning test

--- terraform/vra/automate/outputs.tf
+++ terraform/vra/automate/outputs.tf
@@ -1,16 +1,16 @@
-output "Alcon-automate_public_ip" {
-  value = "${vra7_deployment.automate_machine.*.public_ip}"
-}
+# output "Alcon-automate_public_ip" {
+#   value = "${vra7_deployment.automate_machine.*.public_ip}"
+# }
 
-output "a2_admin" {
- value = "${data.external.a2_secrets.result["a2_admin"]}"
-}
-output "a2_admin_password" {
- value = "${data.external.a2_secrets.result["a2_password"]}"
-}
-output "a2_token" {
- value = "${data.external.a2_secrets.result["a2_token"]}"
-}
-output "a2_url" {
- value = "${data.external.a2_secrets.result["a2_url"]}"
-}
+# output "a2_admin" {
+#  value = "${data.external.a2_secrets.result["a2_admin"]}"
+# }
+# output "a2_admin_password" {
+#  value = "${data.external.a2_secrets.result["a2_password"]}"
+# }
+# output "a2_token" {
+#  value = "${data.external.a2_secrets.result["a2_token"]}"
+# }
+# output "a2_url" {
+#  value = "${data.external.a2_secrets.result["a2_url"]}"
+# }
