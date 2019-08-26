c34ae60268fcb53bb28e3ad1ce5a71db1b608916
Dan Flores <danf425@gmail.com>
2019-08-26 17:00:53 -0400

    remote-exec provisioning test

--- terraform/vra/automate/main.tf
+++ terraform/vra/automate/main.tf
@@ -6,7 +6,7 @@ provider "vra7" {
   insecure = true
 }
 
-resource "vra7_deployment" "machine_automate" {
+resource "vra7_deployment" "automate_machine" {
   count             = "${var.deployment_count}"
   catalog_item_name = "${var.catalog_item_name}"
   description = "${var.deployment_description}"
@@ -14,25 +14,29 @@ resource "vra7_deployment" "machine_automate" {
 ## newly created machine resource specifications
   resource_configuration = {
     CentosOS.cpu = "${var.cpu}"
-#      "${var.blueprint_id}".memory = "4096"
     CentosOS.memory = "${var.memory}"
     CentosOS.storage = "${var.storage}"
+#      "${var.blueprint_id}".memory = "4096"
   }
+
+  connection {
+    user        = "vagrant"
+    private_key = "${file("~/ssh/vagrantdefaultkey")}"
+
 
 # ## provision file, and run deploy-chef-automate.sh
   # provisioner "file" {
   #   content     = "${data.template_file.deploy-chef-automate.rendered}"
   #     destination = "/tmp/deploy-chef-automate.sh"
   # }
-
-  provisioner "remote-exec" {
+    provisioner "remote-exec" {
       inline = [
           "sudo hostname automate-alcon.test",
           # "chmod +x /tmp/deploy-chef-automate.sh",
           # "sudo /tmp/deploy-chef-automate.sh",
       ]
+    }
   }
-
 }
 
 # data "template_file" "deploy-chef-automate" {
