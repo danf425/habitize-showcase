e05749f142472238515aefb7ef6b4f73ed00d034
Ken Langdon <klangdon@chef.io>
2019-09-06 16:44:09 +0000

    refactored Showcase into separate pkgs

---
+++ habitat-packages/Showcase9004/habitat/plan.ps1
@@ -0,0 +1,12 @@
+$pkg_name="Showcase9004"
+$pkg_origin="alcon-poc"
+$pkg_version="9.0.0.4"
+$pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+$pkg_license=@("Apache-2.0")
+$pkg_shasum="TODO"
+$pkg_deps=@()
+$pkg_build_deps=@()
+
+function Invoke-Install {
+ Copy-Item -Path 'C:\Source\Showcase\IBM ShowCase 9.0.0.4' -Destination $pkg_prefix -Recurse -Force
+ }
