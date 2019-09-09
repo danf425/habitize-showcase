d06eb9d3ab310f22e23c79b7df91d64fe22e5c3e
niamhcahill <ncahill@chef.io>
2019-09-09 23:44:03 +0000

    Refactor Showcase Hab Package for separate versions

--- showcase/habitat/plan.ps1
+++ showcase/habitat/plan.ps1
@@ -1,6 +1,6 @@
 $pkg_name="showcase"
 $pkg_origin="alcon-poc"
-$pkg_version="9.0.0.3"
+$pkg_version="9.0.0.4"
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_license=@("Apache-2.0")
 $pkg_shasum="TODO"
@@ -8,5 +8,5 @@ $pkg_deps=@()
 $pkg_build_deps=@()
 
 function Invoke-Install {
- Copy-Item -Path 'C:\Source\Showcase\IBM ShowCase $pkg_version' -Destination $pkg_prefix -Recurse -Force
+ Copy-Item -Path "C:\Source\Showcase\IBM ShowCase $pkg_version" -Destination $pkg_prefix -Recurse -Force
  }
